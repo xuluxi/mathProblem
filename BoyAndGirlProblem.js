a3451a05dda4097cbb482eb543ff78d53e3adf45
xuluxi <xuluxi@meituan.com>
2016-09-01 18:25:42 +0800

    boy and girl problem

---
+++ BoyAndGirlProblem.js
@@ -0,0 +1,40 @@
+/**
+ In a country in which people only want boys every family continues to have children until they have a boy. If they have a girl, they have another child. If they have a boy, they stop. What is the proportion of boys to girls in the country?
+*/
+
+
+var num = 10000,
+    genaNum = 10,
+    boyNum = 0,
+    girlNum = 0;
+
+function born() {
+  return Math.random() < 0.5 ? "boy" : "girl"
+}
+
+function family() {
+  if(born() === "boy"){
+    boyNum++;
+    return;
+  } else {
+    girlNum++;
+    arguments.callee.call(null);
+  }
+}
+
+function genation() {
+  while(num) {
+    family();
+    num--;
+  }
+  var p = parseFloat(girlNum/boyNum);
+  console.log(`proportion of boys to girls is ---> 1 : ${p}`);
+}
+
+while(genaNum) {
+  genation();
+  num = 10000;
+  boyNum = 0;
+  girlNum = 0;
+  genaNum--;
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Use progame to prove math problem
 ## Monty Hall problem 
 see: https://en.wikipedia.org/wiki/Monty_Hall_problem
+
+## Boy And Girl Problem
+In a country in which people only want boys every family continues to have children until they have a boy. If they have a girl, they have another child. If they have a boy, they stop. What is the proportion of boys to girls in the country?
+see: http://mathoverflow.net/questions/17960/google-question-in-a-country-in-which-people-only-want-boys
